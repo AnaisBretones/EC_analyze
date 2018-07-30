fixed brunt vaisala frequency (by masking very low values) + time series for the coupled run until 2100

2300/basic_map.py
@@ -16,11 +16,11 @@ import numpy as np
 import make_plot
 import loading
 
-var = 'psi'
-option = 'Coupled'
+var = 'BruntVF'
+option = 'Uncoupled'
 specificity = ''
-y1 = 2040
-y2 = 2100
+y1 = 1950
+y2 = 2020
 
 comparison = 'no'
 
@@ -44,7 +44,7 @@ class From1950to2100():                                                         
         self.output_file = str(var)+'_map_'+str(self.y1)+'to'\
                            +str(self.y2)+'AnoTo90s_'+str(option)                                #//
      #self.path = '/media/fig010/LACIE SHARE/EC_Earth/EC_data/EC_start_'+str(option)+'.nc'
-     self.path = '/media/fig010/LACIE SHARE/EC_Earth/EC_data/section/psi.nc'#U_'+str(option)+'.nc'
+     self.path = '/media/fig010/LACIE SHARE/EC_Earth/EC_data/bn2_'+str(option)+'.nc'
 
      if compa == 'yes':
       if var == 'temp':                                                                 #//
@@ -76,10 +76,6 @@ class From1950to2100():                                                         
       elif var == 'BruntVF':                                                               #//
         self.vmin = -1100                                                              #//
         self.vmax = -1000                                                             #//
-      else:
-        self.vmin = -0.4*1E7#-0.025
-        self.vmax = 0.4*1E7#0.025
-  
 
      return                                                                             #//
 #//////////////////////////////////////////////////////////////////////////////////////////
@@ -110,7 +106,7 @@ class yearly1950to2100():                                                       
 #//////////////////////////////////////////////////////////////////////////////////////////
 
 
-'''
+
 #months = ['jan','feb','mars','apr','mai','june','july','aug','sept','oct','nov','dec']
 #i_month = months.index(month)
 
@@ -119,20 +115,21 @@ simu = From1950to2100(option,specificity,var,y1,y2,comparison)
 if var == 'ML' or var == 'IceC':
   yr, xr, time = loading.extracting_coord_2D(simu.path)
 else:
-  yr, xr, time, depth = loading.extracting_coord(simu.path,var)
+  yr, xr, time, depth = loading.extracting_coord(simu.path)
 
 VarArray_simu = loading.extracting_var(simu.path, var)
+print(np.shape(time),np.shape(xr),np.shape(VarArray_simu))
 
 index_y1 = np.min(np.where(simu.first_year+time[:]/(3600*24*364.5)>simu.y1))
 index_y2 = np.min(np.where(simu.first_year+time[:]/(3600*24*364.5)>simu.y2))
 
-print(np.shape(VarArray_simu))
+
 if comparison == 'no':
    if var == 'ML' or var == 'IceC':
      make_plot.plot_map(xr,yr,np.mean(VarArray_simu[:,:,index_y1:index_y2+1],axis=2) ,var,title_plot,simu.output_file,simu.vmin,simu.vmax)
    else:
-    print(np.max(VarArray_simu[:,:,index_y1]), np.min(VarArray_simu[:,:,index_y1])) 
-    make_plot.plot_map(xr,yr,VarArray_simu[:,:,index_y1] ,var,title_plot,simu.output_file,simu.vmin,simu.vmax)
+    print(np.max(VarArray_simu[:,:,0,index_y1]), np.min(VarArray_simu[:,:,0,index_y1])) 
+    make_plot.plot_map(xr,yr,VarArray_simu[:,:,0,index_y1] ,var,title_plot,simu.output_file,simu.vmin,simu.vmax)
 
 elif comparison == 'yes':
    index_y1c = np.min(np.where(simu.first_year+time[:]/(3600*24*364.5)>simu.y1_compa))
@@ -141,43 +138,6 @@ elif comparison == 'yes':
      make_plot.plot_map(xr,yr,np.mean(VarArray_simu[:,:,index_y1:index_y2+1],axis=2)-np.mean(VarArray_simu[:,:,index_y1c:index_y2c+1],axis=2) ,var,title_plot,simu.output_file,simu.vmin,simu.vmax)
    else:
      make_plot.plot_map(xr,yr,np.mean(VarArray_simu[mask,0,index_y1:index_y2+1],axis=2)-np.mean(VarArray_simu[mask,0,index_y1c:index_y2c+1],axis=2) ,var,title_plot,simu.output_file,simu.vmin,simu.vma)
-'''
-
-y1 = 2040
-y2 = y1+12
-output_file = str(var)+'_plaquette_'+str(y1)+'To'+str(y2)+'_'+str(option)
-m = Basemap(projection='lcc', resolution='l',
-            lon_0=-20, lat_0=85, lat_1=89.999, lat_2=50,
-            width=1.E7, height=0.9E7)
-   #m = Basemap(projection='ortho',lat_0=60,lon_0=-20,resolution='l')
-
-fig = plt.figure(figsize=(10, 6))
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
 
 
-fig.subplots_adjust(hspace=0.2, wspace=0.000001)
-
-for i in range(1,13):
-   y1 = y1+1   
-   ax = plt.subplot(3, 4, i)
-   ax.set_title(str(y1),color='r')
-   title_plot =  str(specificity)+' '+str(y1)+' to '+str(y1+1)
-
-   simu = From1950to2100(option,specificity,var,y1,y2,comparison)
-   
-   yr, xr, time,depth = loading.extracting_coord(simu.path,var)
-   
-   x,y = m(xr, yr)
-   m.drawcoastlines(linewidth=0.5)
-   m.fillcontinents(color='0.8')
-   
-   VarArray_simu = loading.extracting_var(simu.path, var)
-   
-   index_y1 = np.min(np.where(simu.first_year+time[:]/(3600*24*364.5)>simu.y1))
-   
-   array_to_plot = VarArray_simu[:,:,index_y1]   
-
-   cs = m.pcolor(x,y,array_to_plot,vmin=simu.vmin,vmax=simu.vmax)
-plt.savefig(str(var)+'/'+str(output_file)+'.png',format='png')          
           

2300/loading.py
@@ -43,10 +43,26 @@ def extracting_var(path, variable_name):
     var = Forder(nc.variables['sosaline'][:])
   elif variable_name =='IceC':
     var = Forder(nc.variables['soicecov'][:])
-  elif variable_name =='psi':
-    var = Forder(nc.variables['sobarstf'][:])
+  elif variable_name =='rho':
+    var = Forder(nc.variables['vosigma0'][:])
+  elif variable_name =='BruntVF':
+    var = Forder(nc.variables['vobn2'][:])
+  elif variable_name =='density':
+    var = Forder(nc.variables['sig0'][:])
+  elif variable_name =='Uorth':
+    var = Forder(nc.variables['Uorth'][:])
   nc.close()
   return var
+
+def extracting_coord_1D(path):
+  nc = Dataset(path)
+  xr = Forder(nc.variables['nav_lat'][:])
+  yr = Forder(nc.variables['nav_lon'][:])
+  time = Forder(nc.variables['time_counter'][:])
+  depth = Forder(nc.variables['deptht'][:])
+  X = Forder(nc.variables['X'][:])
+  nc.close()
+  return xr, yr, time, depth, X
 
 def extracting_coord_2D(path):
   nc = Dataset(path)
@@ -56,18 +72,12 @@ def extracting_coord_2D(path):
   nc.close()
   return xr, yr, time
 
-def extracting_coord(path,var):
+def extracting_coord(path):
   nc = Dataset(path)
   xr = Forder(nc.variables['nav_lat'][:])
   yr = Forder(nc.variables['nav_lon'][:])
   time = Forder(nc.variables['time_counter'][:])
-  if var == 'v':
-    depth = Forder(nc.variables['depthv'][:])
-  if var == 'u' or var =='psi':
-    depth = Forder(nc.variables['depthu'][:])
-  else:
-    depth = Forder(nc.variables['deptht'][:])
-
+  depth = Forder(nc.variables['deptht'][:])
   nc.close()
   return xr, yr, time, depth
 
@@ -84,15 +94,32 @@ def regional_subset(var,yr,lat_min,lat_max):
   nj = np.size(var[0,:,0,0])
   nt = np.size(var[0,0,0,:])
   nz = np.size(var[0,0,:,0])
-  var_region = np.zeros((nz,nt,np.size(var[:,:,0,0])))
+  var_region = np.zeros((nz,nt,ni*nj))
+
   n=0
   for j in range(0,nj):
     for i in range(0,ni):
        if yr[i,j]>lat_min and yr[i,j]<lat_max:
           var_region[:,:,n] = var[i,j,:,:]
           n = n+1
-  print(n)
   return var_region[:,:,0:n-1]
+
+def regional_subset_2D(var,yr,lat_min,lat_max):
+  ni = np.size(var[:,0,0])
+  nj = np.size(var[0,:,0])
+  nt = np.size(var[0,0,:])
+  var_region = np.zeros((nt,ni*nj))
+
+  n=0
+  for j in range(0,nj):
+    for i in range(0,ni):
+       if yr[i,j]>lat_min and yr[i,j]<lat_max:
+          var_region[:,n] = var[i,j,:]
+          n = n+1
+  
+  return var_region[:,0:n-1]
+
+# ._._._._._._._._._._._._._._._._._._._._._._._._._._._.
 
 def latitudinal_band_mask(y,lat_min,lat_max):
   mask = ((y>lat_min) & (y<lat_max))
@@ -104,14 +131,32 @@ def land_mask(salinity):
 
 def Ocean_mask(x,y,basin):
   if basin == 'arctic_ocean':
-     mask = (y>81) | ((y>66.3) & ((x>105)|(x<-90) ))
+     mask = (y>81) | ((y>66.3) & ((x>105)|(x<-90) )) 
   elif basin == 'BS_and_KS':
      mask = ((y<80)&(y>68)&(x<105)&(x>20))
   elif basin == 'greenland_sea':
      mask = ( (y<80) & (((y>65)&(x>-25)&(x<-15)) | ((y>76)&(x>0)&(x<20)) | ((x>-15)&(x<0)&(y>76+11*x/15.))))
   elif basin == 'section_ESS':
      mask = ((x>140) & (x<150) & (y>75) & (y<80))
-  elif basin == 'section_FS':
-     mask = ((y<80) & (y>76)&(x>0)&(x<20))
+  elif basin == 'Siberian_seas':
+     mask = ((x>110) & (x<180) & (y>60) & (y<85-5*(x-110)/(180-110.)))
+
+
   return mask
+
+def extract_sea_ice_ext(y1,y2,option):
+
+   txt_file = '/media/fig010/LACIE SHARE/EC_Earth/EC_data/section2300/March_Sea_ice_extent_'+str(y1)+'to'+str(y2)+'_'+str(option)+'.txt'
+   ice_ext_array = np.zeros((y2-y1-1))
+   with open(txt_file, "r") as f:
+     tab = []
+
+     for line in range(0,y2-y1-1):
+         tab.append(f.readline().split())
+     print(tab)
+     ice_ext = np.array([float(x[0]) for x in tab])
+     
+     for i in range(0,y2-y1-1):
+       ice_ext_array[i] = str(ice_ext[i])
+   return ice_ext_array
 

2300/make_plot.py
@@ -102,10 +102,7 @@ def vertical_profile(var1,var2,variable_name,vmin,vmax,z,zmax,ocean,lat_min,name
    elif variable_name == 'density':
      plt.xlabel(r'Density')
    plt.xlim([vmin,vmax])
-   if ocean == 'BS_and_KS':
-      plt.ylim([420,0])
-   else:
-      plt.ylim([z[i_zmax],0])
+   plt.ylim([z[i_zmax],0])
    plt.title(str(ocean.replace("_"," ")))
    if ocean == 'undefined':
       plt.title('Mean Arctic ($>$'+str(lat_min)+'$^{o}$N)')
@@ -146,6 +143,40 @@ def vertical_profile3(var1,var2,var3,y1,y2,variable_name,vmin,vmax,z,zmax,ocean,
    else:
       plt.title(str(ocean.replace("_"," ")))
    plt.savefig(str(variable_name)+'/'+str(name_file.replace(".",""))+'.png')
+   plt.close(fig)
+   return
+
+def one_sec(var,variable_name,x,z,zmax,year,option,vmin,vmax,ocean):
+
+   i_zmax = np.max(np.where(z<zmax))
+
+   plt.figure(figsize=(10,6))
+   plt.rc('text', usetex=True)
+   plt.rc('font', family='serif')
+   fig, ax = plt.subplots()
+   plt.contourf(x,z[0:i_zmax+1],var[0:i_zmax+1,:],40,vmin=vmin,vmax=vmax)#,vmin=31.76,vmax=34.02)
+
+   plt.ylim([np.min(z),z[i_zmax]])
+   plt.ylabel(r'Depth (m)')
+   plt.gca().invert_yaxis()
+   #plt.xticks(tick_locs,tick_lbls)
+   cbar = plt.colorbar()
+   cbar.ax.get_yaxis().labelpad = 15
+   if variable_name == 'temp':
+    cbar.set_label(r'Temperature ($^{o}$C)',fontsize=18)
+   elif variable_name == 'sal':
+    cbar.set_label(r'Salinity (PSU)',fontsize=18)
+   elif variable_name == 'density':
+    cbar.set_label(r'Density',fontsize=18)
+   elif variable_name == 'Uorth':
+    cbar.set_label(r'velocity ortho to the section (cm/s)',fontsize=18)
+
+   if ocean == 'undefined':
+      plt.title('Arctic mediterranean seas ($>$66.34$^{o}$N)')
+   else:
+      plt.title(str(ocean.replace("_"," ")),size=20)
+
+   plt.savefig(str(variable_name)+'/'+str(option.replace(".",""))+'.png')
    plt.close(fig)
    return
 
@@ -203,10 +234,10 @@ def time_serie_two_axis(var,variable_name,t,ice_ext,z,zmax,year,option,vmin,vmax
       mappable = ax1.contourf(year+t/(24*3600*365.),z[0:i_zmax+1],var[0:i_zmax+1,:],40,vmin=vmin,vmax=vmax)#,vmin=31.76,vmax=34.02)
    elif variable_name == 'BruntVF':
       mappable = ax1.contourf(year+t/(24*3600*365.),z[0:i_zmax+1],var[0:i_zmax+1,:],40)#,vmin=31.76,vmax=34.02)
-   if ocean == 'BS_and_KS':
-      plt.ylim([np.min(z),420])
+   if ocean =='BS_and_KS':
+     plt.ylim([np.min(z),430])
    else:
-      plt.ylim([np.min(z),z[i_zmax]])
+     plt.ylim([np.min(z),z[i_zmax]])
    plt.ylabel(r'Depth (m)')
    plt.gca().invert_yaxis()
    cbar = fig.colorbar(mappable, pad=0.15)
@@ -217,11 +248,12 @@ def time_serie_two_axis(var,variable_name,t,ice_ext,z,zmax,year,option,vmin,vmax
    elif variable_name == 'BruntVF':
     cbar.set_label(r'Brunt Vaisala frequency',fontsize=18)
     cbar.formatter.set_powerlimits((0, 0))
-    cbar.update_ticks()   
+    cbar.update_ticks()
+   
    ax2 = ax1.twinx()
-   ax2.plot(year+t/(24*3600*365.),ice_ext,linewidth=3,color="black")
+   ax2.plot(year+t/(24*3600*365.),ice_ext,linewidth=1.5,color="black")
 
-   plt.ylabel(r'Sea ice extent (m$^{2}$)',fontsize=18)
+   plt.ylabel(r'March sea ice extent (m$^{2}$)',fontsize=18)
    # rotate and align the tick labels so they look better
    fig.autofmt_xdate()
    #plt.xticks(tick_locs,tick_lbls)

2300/time_serie_anomalie.py
@@ -19,10 +19,10 @@ import loading
 
 var = 'BruntVF'			# sal, temp, IceC, ML, BruntVF vobn2
 option = 'Coupled'		# Coupled, Uncoupled
-y1 = 2006
-y2 = 2100
+y1 = 1950
+y2 = 2300
 
-basin ='arctic_ocean'		# arctic_ocean, BS_and_KS, greenland_sea, Siberian_seas, undefined
+basin ='BS_and_KS'		# arctic_ocean, BS_and_KS, greenland_sea, Siberian_seas, undefined
 lat_min = 66.34 		#IF basin = 'undefined'
                                 #ex: 66.34 for polar circle
 
@@ -53,7 +53,7 @@ class From1950to2100():									#//
      elif compa == 'yes':
         self.output_file = str(var)+'_TimeSerieAnomaly_'+str(sufix)
 
-     self.path = '/media/fig010/LACIE SHARE/EC_Earth/EC_data/section/T_Coupledbn_arctic.nc'#EC_start_'+str(option)+'3.nc' 
+     self.path = '/media/fig010/LACIE SHARE/EC_Earth/EC_data/section2300/Tbn2'+str(option)+'_arctic.nc' 
 
      if compa == 'yes':
       if var == 'temp':                                                                 #//
@@ -71,7 +71,7 @@ class From1950to2100():									#//
        elif var == 'sal':                                                               #//
         self.vmin = 31.92                                                               #//
         self.vmax = 34.86	                                                        #//
-      elif y1>2007:
+      elif y1>1999:
        if var == 'temp':                                                                #//
         self.vmin = -0.9                                                                #//
         self.vmax = 2.8                                                                 #//
@@ -86,8 +86,8 @@ class From1950to2100():									#//
         self.vmin = 31.92                                                               #//
         self.vmax = 34.86	                                                        #//
        elif var == 'BruntVF':                                                               #//
-        self.vmin = -1.8                                                              #//
-        self.vmax = 0.00012	                                                        #//
+        self.vmin = -100                                                              #//
+        self.vmax = 5	                                                        #//
 
      return										#//
 #//////////////////////////////////////////////////////////////////////////////////////////
@@ -97,8 +97,11 @@ def time_serie_Arctic(path,var,lat_min,basin):
   yr, xr, time, depth = loading.extracting_coord(path)
   TorS = loading.extracting_var(path, var)
   print(np.shape(TorS))
-  S = loading.extracting_var(path,'sal')
-  TorS[np.where( S==0. )] = np.nan
+  if var == 'sal':
+     TorS[np.where( TorS==0. )] = np.nan
+  else:
+     S = loading.extracting_var(path,'sal')
+     TorS[np.where( S==0. )] = np.nan
 
   if basin =='undefined':
      mask = loading.latitudinal_band_mask(yr,lat_min,90)
@@ -150,13 +153,15 @@ if comparison == 'no':
    if var == 'IceC':
      print('ok')
      make_plot.var_fc_time(mean_region_simu[index_y1:index_y2],var,time[index_y1:index_y2],simu.first_year, lat_min,simu.output_file,basin)
+   elif var == 'BruntVF':
+     mean_region_simu[np.where(mean_region_simu < -0.1)] = np.nan
+     make_plot.time_serie_two_axis(mean_region_simu[:,index_y1:index_y2],var,time[index_y1:index_y2],sea_ice_extent,\
+                       depth,simu.max_depth,simu.first_year,simu.output_file,simu.vmin,simu.vmax,basin)
    else:
      #make_plot.time_serie(mean_region_simu[:,index_y1:index_y2],var,time[index_y1:index_y2],ML[index_y1:index_y2],\
      #print(np.shape(sea_ice_extent), np.size(mean_region_simu[0,index_y1:index_y2]))
-     mean_region_simu[np.where(mean_region_simu <-0.1)] = np.nan
-     print(np.shape(sea_ice_extent), np.shape(time[index_y1:index_y2-1]))
      make_plot.time_serie_two_axis(mean_region_simu[:,index_y1:index_y2],var,time[index_y1:index_y2],sea_ice_extent,\
-                       depth,simu.max_depth,simu.first_year,simu.output_file,np.nanmin(mean_region_simu[:,index_y1:index_y2-1]),np.nanmax(mean_region_simu[:,index_y1:index_y2-1]),basin)
+                       depth,simu.max_depth,simu.first_year,simu.output_file,simu.vmin,simu.vmax,basin)
 else:
    index_y1c =np.min(np.where(simu.first_year+time[:]/(3600*24*364.5)>y1_compa))
    index_y2c =np.min(np.where(simu.first_year+time[:]/(3600*24*364.5)>y2_compa))

2300/x_section.py
@@ -19,7 +19,7 @@ import loading
 var = 'Uorth'			# sal, temp, Uorth Utang density
 option = 'Coupled'		# Coupled, Uncoupled
 
-y1 = 2000
+y1 = 2270
 y2 = 2100
 
 basin ='SiberianOrth'		# GSR, SiberianOrth
@@ -51,7 +51,7 @@ class From1950to2100():									#//
      elif compa == 'yes':
         self.output_file = str(var)+'Sept_TimeSerieAnomaly_'+str(sufix)
 
-     self.path = '/media/fig010/LACIE SHARE/EC_Earth/EC_data/section/section_' \
+     self.path = '/media/fig010/LACIE SHARE/EC_Earth/EC_data/section2300/section_' \
                   +str(basin)+'_'+str(option)+'.nc'  
 
      if compa == 'yes':
@@ -86,11 +86,11 @@ class From1950to2100():									#//
           self.vmin = 32.5
           self.vmax = 35.4                                                        #//
        elif var == 'density':                                                                #//
-        self.vmin = 25.                                                               #//
-        self.vmax = 28.                                                                 #//
+        self.vmin = 20.                                                               #//
+        self.vmax = 27.5                                                                 #//
        elif var == 'Uorth':                                                                #//
-        self.vmin = -0.024                                                              #//
-        self.vmax = 0.024                                                                 #//
+        self.vmin = -2.4                                                              #//
+        self.vmax = 2.4                                                                 #//
 
      return										#//
 #//////////////////////////////////////////////////////////////////////////////////////////
@@ -104,7 +104,7 @@ VAR = loading.extracting_var(simu.path, var)
 if var == 'sal':
   VAR[np.where( VAR < 32. )] = np.nan
 elif var == 'density':
-  VAR[np.where( VAR < 24)] =np.nan
+  VAR[np.where( VAR < 18)] =np.nan
 else:
   S = loading.extracting_var(simu.path,'sal')
   VAR[ S==0 ] = np.nan
@@ -115,5 +115,7 @@ make_plot.points_on_map(xr,yr,var,basin)
 
 index_y1 = np.min(np.where(simu.first_year+time[:]/(3600*24*364.5)>simu.y1))
 index_y2 = np.min(np.where(simu.first_year+time[:]/(3600*24*364.5)>simu.y2))
-print(np.shape(VAR), np.shape(depth), np.shape(X))
-make_plot.one_sec(-np.mean(VAR[:,:,index_y1:index_y1+29],axis=2).transpose(),var,X,depth,simu.max_depth,y1,simu.output_file,simu.vmin,simu.vmax,basin)
+if var == 'Uorth':
+  make_plot.one_sec(100*np.mean(VAR[:,:,index_y1:index_y1+29],axis=2).transpose(),var,X,depth,simu.max_depth,y1,simu.output_file,simu.vmin,simu.vmax,basin)
+else:
+  make_plot.one_sec(np.mean(VAR[:,:,index_y1:index_y1+29],axis=2).transpose(),var,X,depth,simu.max_depth,y1,simu.output_file,simu.vmin,simu.vmax,basin)